7e5f72efb3a58a85d61b264d66927908928a32ea
Cole Lipman <lipmancole@gmail.com>
2018-06-20 14:44:07 -0400

    firebase connected

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,46 @@
 var config = {
+  apiKey: "AIzaSyC6NqHL96YCinDDwMXTi8Hv4QRlmBLQ278",
+  authDomain: "colestrainproject.firebaseapp.com",
+  databaseURL: "https://colestrainproject.firebaseio.com",
+  projectId: "colestrainproject",
+  storageBucket: "colestrainproject.appspot.com",
+  messagingSenderId: "233112874834"
+};
+firebase.initializeApp(config);
 
-    apiKey: "AIzaSyC2fXWmf2XHBRMxSm33loaWbt6pISJce-g",
-    authDomain: "chwtimesheet.firebaseapp.com",
-    databaseURL: "https://chwtimesheet.firebaseio.com",
-    projectId: "chwtimesheet",
-    storageBucket: "",
-    messagingSenderId: "163369477025"
+var database = firebase.database();
+
+// import firebase from 'firebase/app'
+// import 'firebase/firestore'
+// import 'firebase/messaging'
+
+function getFormData() {
+
+  //var trainNameText = $("dropdown").find("option:selected").text();
+  var trainName = $(".dropdown").val().trim();
+  var destination = $("#destination").val().trim();
+  var trainTime = $("#train-time").val().trim();
+  var frequency = $("#frequency").val().trim();
+
+  var userInput = {
+    train: trainName,
+    dest: destination,
+    trtime: trainTime,
+    freq: frequency
   };
 
-  firebase.initializeApp(config);
-
-  var database = firebase.database();
-
-function getFormData () {
-    // getting data
-    var trainName = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
-    var trainTime = $("#train-time").val().trim();
-    var frequency = $("#frequency").val().trim();
-
-    var userInput  = {
-        train: trainName,
-        dest: destination,
-        trtime: trainTime
-        freq: frequency
-    };
-    return userInput;
+  return userInput;
 }
+
 
 function submitBtn() {
-
-    event.preventDefault();
-
-   var userInput = getFormData();
-
-    console.log(userInput);
-
-    database.ref("/trains").push(userInput);
-
+  event.preventDefault();
+  var userInput = getFormData();
+  console.log(userInput);
+  database.ref("/trains").push(userInput);
 }
+
+
 
 
 var table = $("<table style='width: 100%'>");
@@ -46,10 +48,18 @@ table.attr("id", "train-table");
 
 var tr = $("<tr>");
 
-var th = $("<th>"); th.text("Train Name"); tr.append(th);
-var th1 = $("<th>"); th1.text("Destination"); tr.append(th1);
-var th2 = $("<th>"); th2.text("Arrival Time"); tr.append(th2);
-var th3 = $("<th>"); th3.text("Frequency"); tr.append(th3);
+var th = $("<th>");
+th.text("Train Name");
+tr.append(th);
+var th1 = $("<th>");
+th1.text("Destination");
+tr.append(th1);
+var th2 = $("<th>");
+th2.text("Arrival Time");
+tr.append(th2);
+var th3 = $("<th>");
+th3.text("Frequency");
+tr.append(th3);
 
 
 table.append(tr);
@@ -60,33 +70,24 @@ $("#train-table-div").append(table);
 
 database.ref("/trains").on("child_added", function(snapshot) {
 
-    console.log(snapshot.val());
+  console.log(snapshot.val());
 
-    var tr = $("<tr>");
+  var tr = $("<tr>");
 
-    var td = $("<td>"); td.text(snapshot.val().name); tr.append(td);
-    var td1 = $("<td>"); td1.text(snapshot.val().role); tr.append(td1);
-    var td2 = $("<td>"); td2.text(snapshot.val().stDate); tr.append(td2);
-    var td3 = $("<td>"); td3.text(snapshot.val().mRate); tr.append(td3);
+  var td = $("<td>");
+  td.text(snapshot.val().train);
+  tr.append(td);
+  var td1 = $("<td>");
+  td1.text(snapshot.val().dest);
+  tr.append(td1);
+  var td2 = $("<td>");
+  td2.text(snapshot.val().);
+  trtime.append(td2);
+  var td3 = $("<td>");
+  td3.text(snapshot.val().freq);
+  tr.append(td3);
 
-
-
-
-
-    var startM = moment(snapshot.val().stDate, "MM/DD/YYYY");
-
-    var monthsWorked = moment().diff(startM, "months");
-
-    console.log(monthsWorked);
-
-    var totalBilled = monthsWorked * parseInt(snapshot.val().mRate);
-
-    console.log(totalBilled);
-
-    var td4 = $("<td>"); td4.text(monthsWorked); tr.append(td4);
-    var td5 = $("<td>"); td5.text(totalBilled); tr.append(td5);
-
-    $("#train-table").append(tr);
+  $("#train-table").append(tr);
 
 })
 
